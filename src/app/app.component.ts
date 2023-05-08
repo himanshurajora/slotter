44a565766f2322b67a59995c3d13e6b9b05d2725
Himanshu Jangid <iamahalofan@gmail.com>
2023-05-08 14:33:08 +0530

    update slotting logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,22 @@ import * as _ from 'lodash';
 import * as moment from 'moment';
 
 interface Slot {
-  days: number[];
-  startTime: string;
-  endTime: string;
+  day: number;
+  hour: string;
 }
+
+export const dayNumberMap = {
+  0: '*',
+  1: '*',
+  2: '*',
+  3: '*',
+  4: '*',
+  5: '*',
+  6: '*',
+};
+
+type DayType = keyof typeof dayNumberMap;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,100 +26,79 @@ interface Slot {
 })
 export class AppComponent {
   vertical = 1;
-  days = [0, 1, 2, 3, 4, 5, 6];
+  days: DayType[] = [0, 1, 2, 3, 4, 5, 6];
   hours = _.reduce(
-    _.range(0, 24),
+    _.range(0, 24, 1),
     (prev: string[], curr: number) => {
       return [...prev, `${curr < 10 ? '0' + curr : curr}:00`];
     },
     []
   );
+  bookedDays: Record<number, boolean> = {};
+  bookedHours: Record<string, boolean> = {};
 
-  slots: Slot[] = [
-    {
-      days: [3, 4],
-      startTime: '00:30',
-      endTime: '12:00',
-    },
-  ];
+  getDay(day: DayType) {
+    return dayNumberMap[day];
+  }
+
+  slots: Slot[] = [];
+
+  // to keep all slots before processing
+  intSlots: Slot[] = [];
 
   currentSlotIndex: number = -1;
   selectedSlot: Slot | undefined;
-  // slots: {
-  //   days: string[];
-  //   startTime: string;
-  //   endTime: string;
-  // }[] = [];
 
   isMouseDown = false;
   isMouseUp = true;
 
-  ngOnInit() {
-    console.log(this.hours, this.days, this.slots);
-  }
+  title = 'slotter';
+
+  ngOnInit() {}
 
   isBooked(
     day: number,
-    days: number[],
+    selectedDay: number,
     hour: string,
-    startTime: string,
-    endTime: string
+    selectedHour: string
   ) {
-    if (days.includes(day)) {
+    if (day === selectedDay) {
       let time = moment(hour, 'HH:mm');
-      let st = moment(startTime, 'HH:mm');
-      let et = moment(endTime, 'HH:mm');
+      let h = moment(selectedHour, 'HH:mm');
 
-      return time.isSameOrAfter(st) && time.isSameOrBefore(et);
+      return time.isSame(h);
     }
 
     return false;
   }
 
-  isInsideSelectedSlot(day: number, hour: string) {
-    if (this.selectedSlot) {
-      console.log('I cam here');
-      return this.isBooked(
-        day,
-        this.selectedSlot.days,
-        hour,
-        this.selectedSlot.startTime,
-        this.selectedSlot.endTime
-      )
-        ? 'selected'
-        : '';
-    }
-
-    console.log
-    return '';
-  }
-
   hasSlot(day: number, hour: string) {
     return _.some(this.slots, (slot) => {
-      return this.isBooked(day, slot.days, hour, slot.startTime, slot.endTime);
+      return this.isBooked(day, slot.day, hour, slot.hour);
     })
       ? 'booked'
       : '';
   }
 
-  getOrCreateSlot(day: number, hour: string, create: boolean = true) {
-    const slot = _.find(this.slots, (slot) => {
-      return this.isBooked(day, slot.days, hour, slot.startTime, slot.endTime);
+  toggleSlot(day: number, hour: string, create: boolean = true) {
+    const slotIndex = _.findIndex(this.slots, (slot) => {
+      return this.isBooked(day, slot.day, hour, slot.hour);
     });
 
-    if (!slot) {
+    if (slotIndex === -1) {
       if (create) {
         this.slots.push({
-          days: [day],
-          startTime: hour,
-          endTime: hour,
+          day,
+          hour,
         });
-        return _.last(this.slots);
+
+        this.selectedSlot = _.last(this.slots);
+        return this.selectedSlot;
       }
       return null;
     }
 
-    return slot;
+    return this.slots.splice(slotIndex, 1)[0];
   }
 
   /**
@@ -120,109 +111,76 @@ export class AppComponent {
     event.stopImmediatePropagation();
     event.stopPropagation();
     this.currentSlotIndex = -1;
-    const slot = this.getOrCreateSlot(day, hour);
+    const slot = this.toggleSlot(day, hour);
     if (slot) {
       this.selectedSlot = slot;
     }
+
+    this.updateFlags();
   }
 
-  selectCurrentSlot(event: MouseEvent, day: number, hour: string) {
-    const slot = this.getOrCreateSlot(day, hour);
-    this.currentSlotIndex = _.indexOf(this.slots, slot);
+  updateFlags() {
+    this.bookedDays = _.reduce(
+      this.slots,
+      (prev, curr) => {
+        return { ...prev, [curr.day]: true };
+      },
+      {}
+    );
+
+    this.bookedHours = _.reduce(
+      this.slots,
+      (prev, curr) => {
+        return {
+          ...prev,
+          [curr.hour]: true,
+        };
+      },
+      {}
+    );
   }
 
-  /**
-   * The function that is mainly responsible for the ui controls
-   * It check the current state of the existing
-   * @param event
-   * @param day
-   * @param hour
-   * @returns
-   */
-  updateSlot(event: MouseEvent, day: number, hour: string) {
-    const slot = this.selectedSlot;
-    if (this.currentSlotIndex === -1 || !slot) return;
+  toggleAllSlotsForADay(day: number) {
+    const allSelectedSlotsForDay = _.filter(this.slots, (slot: Slot) => {
+      return slot.day === day;
+    });
 
-    // handling days
-    const firstDay = _.first(slot.days);
-    const lastDay = _.last(slot.days);
-    const indexOfFirstDay = _.indexOf(this.days, firstDay);
-    const indexOfLastDay = _.indexOf(this.days, lastDay);
-    let indexOfSelectedDay = _.indexOf(this.days, day);
-    while (indexOfSelectedDay < indexOfFirstDay) {
-      slot.days.push(this.days[indexOfSelectedDay++]);
+    // remove all existing slots for day
+    this.slots = _.filter(this.slots, (slot) => {
+      return day !== slot.day;
+    });
+
+    if (allSelectedSlotsForDay.length !== this.hours.length) {
+      this.slots = [
+        ...this.slots,
+        ..._.map(this.hours, (hour) => {
+          return { day, hour };
+        }),
+      ];
     }
-    while (indexOfSelectedDay > indexOfLastDay) {
-      slot.days.push(this.days[indexOfSelectedDay--]);
-    }
 
-    slot.days.sort((a, b) => a - b);
-
-    // handling hours
-    while (moment(hour, 'HH:mm').isBefore(moment(slot.startTime, 'HH:mm'))) {
-      slot.startTime = moment(hour, 'HH:mm')
-        .subtract(1, 'hour')
-        .format('HH:mm');
-    }
-
-    while (moment(hour, 'HH:mm').isAfter(moment(slot.endTime, 'HH:mm'))) {
-      slot.endTime = moment(hour, 'HH:mm').add(1, 'hour').format('HH:mm');
-    }
-
-    // check for overlap
-    this.promptIfOverlap(slot);
-
-    this.currentSlotIndex = -1;
+    this.updateFlags();
   }
 
-  promptIfOverlap(slot: Slot) {
-    // const blocks = [];
+  toggleAllSlotsForHour(hour: string) {
+    const allSelectedSlotsForHour = _.filter(this.slots, (slot: Slot) => {
+      return slot.hour === hour;
+    });
 
-    let i = _.first(slot.days);
-    let n = _.last(slot.days);
+    // remove all existing slots for hour
+    this.slots = _.filter(this.slots, (slot) => {
+      return hour !== slot.hour;
+    });
 
-    console.log('called', i, n);
-
-    // if (!i || !n) return;
-
-    // let startTime = this.mTime(slot.startTime);
-
-    // console.log(startTime.format('HH:mm'));
-
-    // startTime.add(1, 'hour');
-
-    // console.log(startTime.format('HH:mm'));
-
-    // while (i <= n) {
-    //   let startTime = this.mTime(slot.startTime);
-    //   while (startTime.isSameOrBefore(this.mTime(slot.endTime))) {
-    //     blocks.push({ day: i, hour: this.mFormat(startTime) });
-    //     console.log(blocks);
-    //     startTime.add(1, 'hour');
-    //   }
-    //   i++;
-    // }
-
-    for (let s of this.slots) {
-      console.log(s.days, slot.days);
-      if (
-        _.intersection(s.days, slot.days).length > 0 &&
-        this.mTime(slot.startTime).isAfter(this.mTime(s.startTime)) &&
-        this.mTime(slot.endTime).isBefore(this.mTime(s.startTime))
-      ) {
-        alert('Overlap');
-        return;
-      }
+    if (allSelectedSlotsForHour.length !== this.days.length) {
+      this.slots = [
+        ...this.slots,
+        ..._.map(this.days, (day) => {
+          return { day, hour };
+        }),
+      ];
     }
-  }
 
-  mTime(hour: string) {
-    return moment(hour, 'HH:mm');
+    this.updateFlags();
   }
-
-  mFormat(time: moment.Moment) {
-    return time.format('HH:mm');
-  }
-
-  title = 'slotter';
 }
